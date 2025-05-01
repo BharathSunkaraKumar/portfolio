d8c170c49fae5f479865470d0038d791186d1801
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-05-01 15:58:49 +0530

    charngd

--- src/components/projects/Allprojects.jsx
+++ src/components/projects/Allprojects.jsx
@@ -12,41 +12,7 @@ function Allprojects() {
         </div>
         <hr/>
         <div className="projects-container">
-          {
-            data.map((project) => {
-              return <div className='card' key={project.id}>
-                          <div className="card-image">
-                            <img src={project.image} width='100px'/>
-                          </div>
-                          <div className="card-title">
-                            <h4>{project.title}</h4>
-                          </div>
-                          <div className="card-tech">
-                            {
-                              project.tech.map((e, i) => {
-                                return <span key={i}>{e}</span>
-                              })
-                            }
-                          </div>
 
-                          <div className="links-ul">
-                            <ul className="card-links">
-                              {console.log(project.navLinks)}
-                              {
-                                project.navLinks.map((link, i) => {
-                                  console.log(link)
-                                  return <li className='li-link' key={i}>
-                                    <p>{link}</p>
-                                    <span>{link !== 'view' && <OpenInNewIcon/>}</span>
-                                  </li>
-                                })
-                              }
-                            </ul>
-                          </div>
-                          
-                     </div>
-            })
-          }
         </div>
     </div>
   )
